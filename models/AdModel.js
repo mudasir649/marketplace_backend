ab5fc106fc60a9b2c50f0a8a3ee474981b729128
Eidcarosse <eidcarosse@gmail.com>
2023-11-21 16:46:28 +0500

    phone number

--- models/AdModel.js
+++ models/AdModel.js
@@ -1,127 +1,134 @@
 import mongoose from "mongoose";
-const AdSchema = new mongoose.Schema({
+const AdSchema = new mongoose.Schema(
+  {
     userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
     images: [
-        {
-            type: String,
-            default: null
-        }
-    ], 
-    category: {
+      {
         type: String,
-        default: null
+        default: null,
+      },
+    ],
+    category: {
+      type: String,
+      default: null,
     },
     subCategory: {
-        type: String,
-        default: null
-    }, 
-    title: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
-    price:{
-        type: Number,
-        default: null
+    title: {
+      type: String,
+      default: null,
+    },
+    price: {
+      type: Number,
+      default: null,
     },
     minPrice: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     maxPrice: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     condition: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     brand: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     videoUrl: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     description: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
+    },
+    phone: {
+      type: Number,
+      default: null,
     },
     whatsapp: {
-        type: Number,
-        default: null
+      type: Number,
+      default: null,
     },
     viber: {
-        type: Number,
-        default: null
+      type: Number,
+      default: null,
     },
     website: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     address: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     model: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     year: {
-        type: Number,
-        default: null
+      type: Number,
+      default: null,
     },
     bodyShape: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     gearBox: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     fuelType: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     exteriorColor: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     interiorColor: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     engineCapacity: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     cylinder: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     km: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     axeltype: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     views: {
-        type: Number,
-        default:0
+      type: Number,
+      default: 0,
     },
     latitude: {
-        type: Number
+      type: Number,
     },
     longitude: {
-        type: Number
-    }
-}, { timestamps: true });
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
 
-const Ad = mongoose.model('Ad', AdSchema);
+const Ad = mongoose.model("Ad", AdSchema);
 
 export default Ad;
