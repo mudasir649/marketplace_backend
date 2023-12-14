10bb64bf8c612a3351709cf1a41ebb0995a6c6fd
usamakhan <usamakhan@192.168.1.6>
2023-12-14 19:30:46 +0500

    secure url added to mutiple Images

--- utils/uploadImage.js
+++ utils/uploadImage.js
@@ -1,66 +1,66 @@
 import { v2 as cloudinary } from "cloudinary";
 
 // Set up Cloudinary configuration
-const cloud_name = 'dlkuyfwzu';
-const api_key = '573974714511519';
-const api_secret = 'FKTt8caSw6fXPU7sfD9maqMWuUA'
+const cloud_name = "dlkuyfwzu";
+const api_key = "573974714511519";
+const api_secret = "FKTt8caSw6fXPU7sfD9maqMWuUA";
 
 cloudinary.config({
-    cloud_name: cloud_name,
-    api_key: api_key,
-    api_secret: api_secret,
-    secure: true
+  cloud_name: cloud_name,
+  api_key: api_key,
+  api_secret: api_secret,
+  secure: true,
 });
 
 const uploadSingleImage = async (files) => {
   const imageBuffer = files.data;
   const uploadOptions = {
-    resource_type: 'image',
-    secure: true
+    resource_type: "image",
+    secure: true,
   };
 
   return new Promise((resolve, reject) => {
-    cloudinary.uploader.upload_stream(uploadOptions, (error, result) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(result.secure_url);
-      }
-    }).end(imageBuffer);
-  })
-
-}
-
-
-const uploadMultipleImage =  async (files) => {
-    const uploadPromises = files.map(file => {
-        const imageBuffer = file.data;
-        const uploadOptions = {
-          resource_type: 'image',
-        };
-    
-        // Return a promise for each image upload
-        return new Promise((resolve, reject) => {
-          cloudinary.uploader.upload_stream(uploadOptions, (error, result) => {
-            if (error) {
-              reject(error);
-            } else {
-              resolve(result.url);
-            }
-          }).end(imageBuffer);
-        });
-      });
-    
-      try {
-        // Upload all images and wait for results
-        const imageUrls = await Promise.all(uploadPromises);
-        return imageUrls;
-      } catch (error) {
-        return error;
-    }
-}
-
-export {
-  uploadMultipleImage,
-  uploadSingleImage
+    cloudinary.uploader
+      .upload_stream(uploadOptions, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result.secure_url);
+        }
+      })
+      .end(imageBuffer);
+  });
 };
+
+const uploadMultipleImage = async (files) => {
+  const uploadPromises = files.map((file) => {
+    const imageBuffer = file.data;
+    const uploadOptions = {
+      resource_type: "image",
+      secure: true,
+    };
+
+    // Return a promise for each image upload
+    return new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(uploadOptions, (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result.secure_url);
+          }
+        })
+        .end(imageBuffer);
+    });
+  });
+
+  try {
+    // Upload all images and wait for results
+    const imageUrls = await Promise.all(uploadPromises);
+    return imageUrls;
+  } catch (error) {
+    return error;
+  }
+};
+
+export { uploadMultipleImage, uploadSingleImage };
